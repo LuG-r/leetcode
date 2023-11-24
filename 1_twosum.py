1290c0af71d64db39d87e3f83626196df06e00dd
Lucas Gracia <lucasejgracia@gmail.com>
2023-11-24 17:22:37 +0000

    old solutions removed

--- 1_twosum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-   def twoSum(self, nums: List[int], target: int) -> List[int]:
-       positions = {}
-
-       for index, value in enumerate(nums):
-           if target - value in positions.keys():
-               return [index, positions[target - value]]
-           else:
-               positions[value] = index
